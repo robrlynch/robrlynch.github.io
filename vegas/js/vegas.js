557d76a588845dcd7ef7009f1fa6e712908dc527
robrlynch <34169074+robrlynch@users.noreply.github.com>
2019-05-14 14:39:16 -0500

    Update vegas.js

--- vegas/js/vegas.js
+++ vegas/js/vegas.js
@@ -1,3 +1,11 @@
 var numPlayers = prompt('how many players today');
 numPlayers = parseInt(numPlayers);
 document.write(numPlayers *4);
+if (numPlayers=4) {
+    var Player1 = prompt('Player1 First Name');
+    var Player2 = prompt('Player2 First Name');
+    var Player3 = prompt('Player3 First Name');
+    var Player4 = prompt('Player4 First Name');
+    }
+
+document.write('<h1>Hi ' Player1 + ', ' + Player2 + ', ' + Player3 + ', ' + Player4 '</h1>');
