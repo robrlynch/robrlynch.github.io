d05962e9fcce3abf62f006982c025a12039c4d1b
robrlynch <34169074+robrlynch@users.noreply.github.com>
2019-05-15 11:13:04 -0500

    Update vegas.js

--- vegas/js/vegas.js
+++ vegas/js/vegas.js
@@ -10,5 +10,5 @@ if (numPlayers=4) {
 
 
 document.write(Player1);
-document.write('<h1>Hi! '+ Player1 + Player2 + ' </h1>');
+document.write('<h1>Hi '+ Player1 ', '+ Player2 + ', ' + Player3 + ', ' + Player4 +'!!! </h1>');
 //Player1 + ', ' + Player2 + ', ' + Player3 + ', ' + Player4 
